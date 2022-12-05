efbf2a529af41255713d2318512ddd2037f1e5bf
Nikola Occidentale <nikolaoccid@gmail.com>
2022-12-05 16:22:48 +0100

    Reconfigure for Vercel deploy

---
+++ api/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("source-map-support/register");
+require("reflect-metadata");
+require("./server");
+//# sourceMappingURL=index.js.map
